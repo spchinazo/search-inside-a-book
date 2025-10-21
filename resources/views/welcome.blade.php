c4134184b4ab0ce5d10676d6872c5bc5ab838d2d
Franco Gilio <franco.gilio@gmail.com>
2025-10-21 12:17:42 +0100

    chore: migrate build tooling to vite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+    @vite(['resources/js/app.js'])
+
     <!-- Styles -->
     <style>
         html, body {
