4327661ce335c70037b6340c5c985a9c5d204a43
Victor Messias <vic_silva@live.com>
2025-11-03 11:41:31 -0300

    [CHANGED] - Page title

--- resources/views/book-search.blade.php
+++ resources/views/book-search.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Busca no Livro - Eloquent JavaScript</title>
+    <title>Book Search</title>
     
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
