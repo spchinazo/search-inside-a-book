0ee0c6d2f3dace4dd918826cf7a2b7ea76765e91
Marcos A Medina <mamedina.pixon@gmail.com>
2026-01-06 19:07:10 +0000

    feat: Integrate Laravel Scout and Meilisearch for book page search.

--- app/BookPage.php
+++ app/BookPage.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class BookPage extends Model
 {
@@ -11,6 +12,23 @@ class BookPage extends Model
         'page_number',
         'content',
     ];
+
+    use Searchable;
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array<string, mixed>
+     */
+    public function toSearchableArray(): array
+    {
+        return [
+            'id' => (int) $this->id,
+            'book_id' => (int) $this->book_id,
+            'page_number' => (int) $this->page_number,
+            'content' => $this->content,
+        ];
+    }
 
     public function book()
     {
