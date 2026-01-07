650755b5628b7634189883ff26e11baee6f9b733
Marcos A Medina <mamedina.pixon@gmail.com>
2026-01-07 10:45:36 +0000

    feat: add API endpoint and controller for searching book pages using Laravel Scout

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\BookController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,3 +13,7 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::prefix('books')->group(function () {
+    Route::get('{book}/search', [BookController::class, 'search']);
+});
